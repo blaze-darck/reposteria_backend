4b244d64044a7dcad0e9b97a28922e01cbd249b9
blaze-darck <paredesgabriel784@gmail.com>
2025-11-11 00:39:43 -0400

    feat:Adicion del modulo de pagos del sitema

---
+++ src/pagos/entities/Pago.ts
@@ -0,0 +1,25 @@
+import { Entity, ManyToOne, Column } from "typeorm";
+import { BaseEntityAudit } from "../../auditoria/entities/datosAuditoria";
+import { Pedido } from "../../pedidos/entities/Pedido";
+
+@Entity()
+export class Pago extends BaseEntityAudit {
+  @ManyToOne(() => Pedido, (pedido) => pedido.pagos, { onDelete: "CASCADE" })
+  pedido!: Pedido;
+
+  @Column({ type: "datetime", nullable: true })
+  fechaPago?: Date;
+
+  @Column("decimal", { precision: 10, scale: 2 })
+  monto!: number;
+
+  @Column({
+    type: "enum",
+    enum: ["efectivo", "tarjeta", "transferencia"],
+    nullable: true,
+  })
+  metodoPago?: "efectivo" | "tarjeta" | "transferencia";
+
+  @Column({ default: false })
+  confirmado!: boolean;
+}
